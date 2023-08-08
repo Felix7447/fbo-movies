2096528d4856518e3730f41e73bd2ec4a8c8b45d
Felix Reyna <felixreyna48@gmail.com>
2023-08-08 10:21:43 -0400

    feat: :sparkles: Added UI to account page

---
+++ app/_components/ProfileCircle.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from "@/styles/profile.module.scss"
+
+const ProfileCircle = () => {
+  return (
+    <>
+      <main className={styles.main}>
+        <span>N</span>
+      </main>
+      <footer className={styles.footer}>Name</footer>
+    </>
+  )
+}
+
+export default ProfileCircle
--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import styles from '@/styles/page.module.scss'
+import account_styles from '@/styles/accountPage.module.scss'
+import ProfileCircle from '../_components/ProfileCircle'
 
 const Account = () => {
   return (
     <section className={styles.main}>
-      Account
+      <main className={account_styles.account_main}>
+        <article>
+          <ProfileCircle />
+        </article>
+      </main>
+      <article className={account_styles.article}>
+        <h2>My list</h2>
+        <footer>
+        </footer>
+      </article>
     </section>
   )
 }
